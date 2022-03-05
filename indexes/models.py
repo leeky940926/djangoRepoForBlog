10f2f38870f764d84e7f6bf5d897e277793bc7e8
kylee <leeky940926@gmail.com>
2022-03-05 16:08:17 +0900

    Indexes Setting

---
+++ indexes/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['last_name', 'first_name']),
+            models.Index(fields=['first_name'], name='first_name_idx'),
+        ]
