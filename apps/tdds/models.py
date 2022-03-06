d2c78e0edecb8f9161a27f31c80991647f06ae54
kylee <leeky940926@gmail.com>
2022-03-06 14:31:56 +0900

    TDD를 위한 모델링

--- apps/tdds/models.py
+++ apps/tdds/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Role(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class User(models.Model):
+    role = models.ForeignKey('tdds.Role', on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=500)
